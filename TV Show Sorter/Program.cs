6f07f1f463f878ad2a18fae4b54e9a18ff92a3a3
Ben Haycraft <haycraft2@gmail.com>
2019-07-24 17:44:17 -0500

    Added ablility to change the output folder

--- TV Show Sorter/Program.cs
+++ TV Show Sorter/Program.cs
@@ -9,11 +9,27 @@ namespace TV_Show_Sorter
     class Program
     {
 
-        public static string RootDir = ConfigurationManager.AppSettings["RootDir"];
+        public static string SearchFolder = ConfigurationManager.AppSettings["SearchFolder"];
+        public static string DestinationFolderConfig = ConfigurationManager.AppSettings["DestinationFolder"];
+        public static string DestinationFolder;
+        public static string EnableOutFolder = ConfigurationManager.AppSettings["EnableOutFolder"].ToLower();
+
         static Regex tvShow = new Regex(@"s\d\d");
         static Regex ShowNameRegex = new Regex(@".*?(?=\ss\d\d)");
         public static string seasonNumber;
         public static string showName;
+
+        private static void SetDestinationFolder()
+        {
+            if (EnableOutFolder == "false")
+            {
+                DestinationFolder = SearchFolder;
+            }
+            else if (EnableOutFolder == "true")
+            {
+                DestinationFolder = DestinationFolderConfig;
+            }
+        }
 
         private static string ShowMatches(Regex r, Match m)
         {
@@ -38,10 +54,9 @@ namespace TV_Show_Sorter
             return null;
         }
 
-
         private static void SearchFolders()
         {
-            foreach (string file in Directory.GetFiles(RootDir, "*.*", SearchOption.AllDirectories))
+            foreach (string file in Directory.GetFiles(SearchFolder, "*.*", SearchOption.AllDirectories))
             {
                 string fileNoExt = Path.GetFileNameWithoutExtension(file);
                 Match matches = tvShow.Match(fileNoExt);
@@ -57,7 +72,7 @@ namespace TV_Show_Sorter
                     ShowMatches(tvShow, matches);
                     ShowName(ShowNameRegex, matchName);
                     Console.WriteLine(showName);
-                    string showFolder = Path.Combine(RootDir, showName);
+                    string showFolder = Path.Combine(DestinationFolder, showName);
                     string seasonFolder = Path.Combine(showFolder, "Season " + seasonNumber);
                     string newFile = (seasonFolder + "\\" + filename);
                     if (!Directory.Exists(showFolder))
@@ -90,12 +105,14 @@ namespace TV_Show_Sorter
         }
 
 
-
         static void Main()
         {
             while (true)
             {
+                SetDestinationFolder();
                 SearchFolders();
+                Console.WriteLine("");
+                Console.WriteLine("Finished");
                 Console.ReadKey();
                 Console.Clear();
             }
